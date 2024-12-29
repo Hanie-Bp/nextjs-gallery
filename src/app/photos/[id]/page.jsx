6e54bdba8b03d0b577d6605933c20b25868eafc4
Hanie Bp <haniebaratpoor@gmail.com>
2024-12-29 13:00:51 +0330

    add code

--- src/app/photos/[id]/page.jsx
+++ src/app/photos/[id]/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import images from "../../../../public/photos";
 import PhotoCard from "@/components/photoCard";
 
-// console.log(images);
+
 
 const page = ({ params }) => {
   const photo = images.find((photo) => photo.id === Number(params.id));
